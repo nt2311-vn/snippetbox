26f5164c8fc497849d5444423398bb239b5b7e5c
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-18 15:36:41 +0700

    add flag to bind address to serve on http network

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -9,17 +10,19 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
 	fileServer := http.FileServer(http.Dir(filepath.Join("ui", "static", "/")))
 
+	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", handlers.Home)
 	mux.HandleFunc("/snippet/view", handlers.SnippetView)
 	mux.HandleFunc("/snippet/create", handlers.SnippetCreate)
 
-	log.Println("Starting server on :4000")
-	if err := http.ListenAndServe(":4000", mux); err != nil {
+	log.Printf("Starting server on %s\n", *addr)
+	if err := http.ListenAndServe(*addr, mux); err != nil {
 		log.Fatalln("error on starting server", err)
 	}
 }
