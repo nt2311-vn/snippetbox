0c4819c8e149a1ed94167ad17b286e53fcce5587
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-18 13:52:46 +0700

    update static home page and main function of web

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/nt2311-vn/snippetbox/internal/handlers"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", handlers.Home)
+	mux.HandleFunc("/snippet/view", handlers.SnippetView)
+	mux.HandleFunc("/snippet/create", handlers.SnippetCreate)
 
 	log.Println("Starting server on :4000")
 	if err := http.ListenAndServe(":4000", mux); err != nil {
