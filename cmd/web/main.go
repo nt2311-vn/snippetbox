fa95c45969009e2df4ba0ba5c5cb0e3551ff6aaa
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-18 11:24:00 +0700

    add 2 more routes on view and create of snippet

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 	if err := http.ListenAndServe(":4000", mux); err != nil {
@@ -15,6 +17,14 @@ func main() {
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Hello from snippet box"))
+}
+
+func snippetView(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
