74d2a706df9cf0ddf1e51432a22b75b363ea8331
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-20 10:09:04 +0700

    write query return multiple records limit 10

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -54,5 +54,32 @@ func (sm *SnippetModel) Get(id int) (*Snippet, error) {
 }
 
 func (sm *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	query := `select id, title, content, created, expires from snippets
+	where expires > utc_timestamp() order by id desc limit 10
+	`
+
+	rows, err := sm.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+	for rows.Next() {
+		s := &Snippet{}
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, err
 }
