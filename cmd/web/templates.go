d30a4cb0772742f0eb3190b0a987f6ecfa43ae91
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-20 11:50:19 +0700

    write new template cache function to not executeTemplate repeatably

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,40 @@
 package main
 
-import "github.com/nt2311-vn/snippetbox/internal/models"
+import (
+	"path/filepath"
+	"text/template"
+
+	"github.com/nt2311-vn/snippetbox/internal/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(filepath.Join(htmlStaticDir, "*.html"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		files := []string{
+			filepath.Join(htmlStaticDir, "base.html"),
+			filepath.Join(htmlPartialDir, "nav.html"),
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
