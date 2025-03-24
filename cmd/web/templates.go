0e47962be72397489444fac7b06c6d3590d1b3b0
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-24 13:51:27 +0700

    add flash to session manaager when init new template

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -25,9 +25,10 @@ type templateData struct {
 	Flash       string
 }
 
-func (app *application) newTemplateData(_ *http.Request) *templateData {
+func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
