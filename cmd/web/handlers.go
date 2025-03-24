e849f5b6026e86efdd30006be9fd55fb96c3e4c7
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-24 10:52:42 +0700

    initialize the form struct when view the create snippet route

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -65,6 +65,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+
+	data.Form = snippetCreateForm{Expires: 365}
 	app.render(w, http.StatusOK, "create.html", data)
 }
 
