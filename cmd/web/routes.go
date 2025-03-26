4c19deb171840e04437a8f8d91d3ea4f25a5f723
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-26 13:37:12 +0700

    update handle the embed file

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,7 +16,7 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
