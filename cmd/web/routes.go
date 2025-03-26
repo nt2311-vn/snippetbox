a96a7e3048f98886642608c7ba73dfdd2300b67b
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-26 10:42:35 +0700

    protect the the application by adding nosurf middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,7 +20,7 @@ func (app *application) routes() http.Handler {
 
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
 
 	// Authentication
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
