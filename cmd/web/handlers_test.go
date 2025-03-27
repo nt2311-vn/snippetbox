d13fcc780a09b29778ced516df3cf2acf61119a2
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-27 14:52:21 +0700

    update handlers test using testsutils package

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"io"
 	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/nt2311-vn/snippetbox/internal/assert"
@@ -17,22 +15,10 @@ func TestPing(t *testing.T) {
 		infoLog:  log.New(io.Discard, "", 0),
 	}
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-	defer rs.Body.Close()
-
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	code, _, body := ts.get(t, "/ping")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
